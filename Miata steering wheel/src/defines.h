690486ba23c3331fd0ca50bb22cee7793435b9be
HexRebuilt <32871843+HexRebuilt@users.noreply.github.com>
2021-07-12 18:10:22 +0200

    button mapping
    added defines

---
+++ Miata steering wheel/src/defines.h
@@ -0,0 +1,20 @@
+#define DEFAULT_DELAY 500
+
+//list of buttons and pins
+#define MUTEBUTTON  PH6 //D9
+#define SKIP_BUTTON PH5 //D8
+#define BACK_BUTTON PH4 //D7
+#define CALL_ON     PH3 //D6
+#define CALL_OFF    PG5 //D4
+#define VOICE_CMD   PE5 //D3
+#define SOURCE      PE4 //D2 to be defined
+#define PAUSE       PE3 //D3
+#define VOL_UP
+#define VOL_DOWN
+
+//list of process costants
+#define RADIO_BUTTONS   10 //total # of radio input
+
+//list of output pins
+#define RADIO_ANALOGUE PF0  //A0
+
--- Miata steering wheel/src/main.cpp
+++ Miata steering wheel/src/main.cpp
@@ -1,10 +1,38 @@
 #include <Arduino.h>
+#include "defines.h"
+#include <string.h>
 
+int buttons[RADIO_BUTTONS]={MUTEBUTTON, SKIP_BUTTON, BACK_BUTTON, CALL_ON, CALL_OFF, VOICE_CMD, PAUSE,0,0};
 
 void setup() {
   // put your setup code here, to run once:
+  
+  //push-button configuration
+  for (int i = 0; i < RADIO_BUTTONS-2; i++) //exclude the volume ones
+  {
+    pinMode(buttons[i],INPUT);
+  }
+
+
+  Serial.begin(9600);
+  delay(100);
+  if (Serial.available())
+  {
+    Serial.println("Pin configuration DONE");
+  }
+  
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  //int sensorValue = digitalRead(MUTEBUTTON); //gives a 0 if it is low and 1 if high
+  
+  
+  
+  //Serial.println(sensorValue);
+
+
+  delay(DEFAULT_DELAY);
 }
